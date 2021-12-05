05966f1887c6a725ec593c691e8abc1da6feeee6
X-CoTang <xcotang@gmail.com>
2021-12-05 18:59:18 +0700

    first commit

---
+++ admin/commons/nav_menu.php
@@ -0,0 +1,69 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand" href="http://laptopcu.com/">Laptopcu.com</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul class="navbar-nav">
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Nguời dùng
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/users/add.php">Thêm</a>
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/users">Xem</a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Sản phẩm
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/product/add.php">Thêm</a>
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/product">Xem</a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Thương Hiệu
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/brand/add.php">Thêm</a>
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/brand">Xem</a>
+                </div>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Thể loại
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/cate/add.php">Thêm</a>
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/cate">Xem</a>
+                </div>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    Slider
+                </a>
+                <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/slider/add.php">Thêm</a>
+                    <a class="dropdown-item" href="http://laptopcu.com/admin/slider">Xem</a>
+                </div>
+            </li>
+            <li class="nav-item ">
+                <a class="nav-link " href="http://laptopcu.com/admin/order">
+                    QLi don hang
+                </a>
+            </li>
+            <li class="nav-item logout">
+                <a class="nav-link " href="?logout">
+                    Logout
+                </a>
+            </li>
+        </ul>
+    </div>
+</nav>
